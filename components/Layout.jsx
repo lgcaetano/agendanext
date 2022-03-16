61375022fe39d49b2cf5af17f358c716f31ed892
Luis Gabriel Caetano Diniz <luisgabrielcaetano@gmail.com>
2022-03-15 21:10:57 -0300

    fixing minor bugs

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,8 +5,7 @@ export default function Layout(props){
     return (
         <div className="w-full min-h-screen bg-blue-200 font-sans flex flex-col">
             <Head>
-                {/* <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" /> */}
-                <script src="https://kit.fontawesome.com/fab4145b10.js" crossorigin="anonymous"></script>
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous" />
             </Head>
             <TopBar></TopBar>
             <div className="flex flex-col justify-around items-center my-auto">
